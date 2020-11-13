c2998b2dced09f7e8c77822f5dafb84a77649c41
JeffersGlass <glass.jeffrey@gmail.com>
2020-11-12 20:43:45 -0600

    Add atest, btest

--- relayTest_new.py
+++ relayTest_new.py
@@ -4,67 +4,111 @@ import sys, os, time
 import ctypes
 import logging
 
-from relayUtils import *
+from relayUtils import relayBoard
+
+def atest():
+        relays = relayBoard()
+        relays.loadLib()
+        ids = relays.enumDevs()
+        if len(ids) > 0:
+                relays.openDevById(ids[0])
+        else: exit("No relay board found")
+
+        relays.openAllRelays()
+        relays.closeRelay(4)
+        time.sleep(15)
+        relays.openRelay(4)
+        relays.closeRelay(3)
+        time.sleep(20)
+        relays.openRelay(3)
+        relays.closeRelay(4)
+        time.sleep(15)
+
+        relays.openAllRelays()
+        relays.closeDev()
+        relays.unloadLib()
+
+def btest():
+        relays = relayBoard()
+        relays.loadLib()
+        ids = relays.enumDevs()
+        if len(ids) > 0:
+                relays.openDevById(ids[0])
+        else: exit("No relay board found")
+
+        light = [2,3,4]
+
+        for l in light:
+                relays.closeRelay(l)
+                relays.blinkRelay(l)
+        time.sleep(20)
+        for l in light:
+                relays.openRelay(l)
+                relays.noBlink(l)
+
+        relays.openAllRelays()
+        relays.closeDev()
+        relays.unloadLib()
 
 def ctest():
-	relays = relayBoard()
-	relays.loadLib()
-	ids = relays.enumDevs()
-	if len(ids) > 0:
-		relays.openDevById(ids[0])
-	else: exit("No relay board found")
-	
-	relays.closeRelay(1)
-	time.sleep(1)
-	relays.openRelay(1)
-	time.sleep(1)
+        relays = relayBoard()
+        relays.loadLib()
+        ids = relays.enumDevs()
+        if len(ids) > 0:
+                relays.openDevById(ids[0])
+        else: exit("No relay board found")
+        
+        relays.closeRelay(1)
+        time.sleep(1)
+        relays.openRelay(1)
+        time.sleep(1)
 
-	relays.closeAllRelays()
-	time.sleep(1)
-	relays.openAllRelays()
-	time.sleep(1)
-	
-	relays.blinkRelay(1)
-	time.sleep(10)
+        relays.closeAllRelays()
+        time.sleep(1)
+        relays.openAllRelays()
+        time.sleep(1)
+        
+        relays.blinkRelay(1)
+        time.sleep(10)
 
-	relays.blinkRelay(2)
-	time.sleep(10)
+        relays.blinkRelay(2)
+        time.sleep(10)
 
-	relays.blinkRelay(3)
-	time.sleep(10)
-	
-	relays.noBlink(1)
-	time.sleep(5)	
-	relays.openAllRelays()
-	relays.closeDev()
-	relays.unloadLib()
-	
+        relays.blinkRelay(3)
+        time.sleep(10)
+        
+        relays.noBlink(1)
+        time.sleep(5)   
+        relays.openAllRelays()
+        relays.closeDev()
+        relays.unloadLib()
+        
  
 
 def main():
-	logging.basicConfig(level=logging.DEBUG)
+        logging.basicConfig(level=logging.DEBUG)
 
-	print("Starting test")
-	ctest()
-	print("Ending test")
+        print("Starting test")
+        atest()
+        print("Ending test")
 
 
 '''def main():
-	print("Test 4-ch relay")
-	loadLib()
-	getLibFunctions()
-	try:
-		print("Searching for compatible devices")
-		global devids
-		devids = enumDevs()
-		if len(devids) != 0 :
-			# Test any 1st found dev .
-			print("Testing relay with ID=" + devids[0])
-			openDevById(devids[0])
-			ctest()
-			closeDev()
-	finally:  
-		unloadLib()'''
+        print("Test 4-ch relay")
+        loadLib()
+        getLibFunctions()
+        try:
+                print("Searching for compatible devices")
+                global devids
+                devids = enumDevs()
+                if len(devids) != 0 :
+                        # Test any 1st found dev .
+                        print("Testing relay with ID=" + devids[0])
+                        openDevById(devids[0])
+                        ctest()
+                        closeDev()
+        finally:  
+                unloadLib()'''
 
 if __name__ == "__main__" :
   main()
